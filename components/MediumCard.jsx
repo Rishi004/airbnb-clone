1c037ba1efd749885989a1ed5783d8eb660cdca6
Rishi004 <rishireiko@gmail.com>
2023-12-08 17:24:36 +0530

    first commit

---
+++ components/MediumCard.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import React from "react";
+
+function MediumCard({ img, title }) {
+    return (
+        <div className="cursor-pointer hover:scale-105 transform transition duration-300 ease-out">
+            <div className="relative h-80 w-80">
+                <Image src={img} alt="live-img" layout="fill" />
+            </div>
+            <h2 className="text-2xl mt-3">{title}</h2>
+        </div>
+    );
+}
+
+export default MediumCard;
